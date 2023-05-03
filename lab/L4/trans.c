34efaa2f3b75aa43a2ae1f4c854411d2d8da16b6
JohnFrankz <86246881+JohnFrankz@users.noreply.github.com>
2023-05-03 13:59:39 +0800

    Finish lab4

--- lab/L4/trans.c
+++ lab/L4/trans.c
@@ -19,23 +19,6 @@ int is_transpose(int M, int N, int A[N][M], int B[M][N]);
  *     searches for that string to identify the transpose function to
  *     be graded. 
  */
-char transpose_submit_desc[] = "Transpose submission";
-void transpose_submit(int M, int N, int A[N][M], int B[M][N])
-{
-    if (M == 32) {
-        transpose_32(M, N, A, B);
-    } else if (M == 64) {
-        transpose_64(M, N, A, B);
-    } else {
-        transpose_61(M, N, A, B);
-    }
-}
-
-/* 
- * You can define additional transpose functions below. We've defined
- * a simple one below to help you get started. 
- */ 
-char transpose_32_desc[] = "Transpose 32x32";
 void transpose_32(int M, int N, int A[N][M], int B[M][N]) {
     int blockSize = 8;
     int en = blockSize * (M / blockSize);
@@ -69,12 +52,81 @@ void transpose_32(int M, int N, int A[N][M], int B[M][N]) {
 }
 
 void transpose_64(int M, int N, int A[N][M], int B[M][N]) {
+    int a0, a1, a2, a3, a4, a5, a6, a7;
+    int i, j, ii, jj;
+    for (i = 0; i < 64; i += 8) {
+        for (j = 0; j < 64; j+= 8) {
+            for (ii = i; ii < i + 4; ii++) {
+                a0 = A[ii][j]; a1 = A[ii][j + 1]; a2 = A[ii][j + 2]; 
+                a3 = A[ii][j + 3]; a4 = A[ii][j + 4]; a5 = A[ii][j + 5];
+                a6 = A[ii][j + 6]; a7 = A[ii][j + 7];
 
+                B[j][ii] = a0; B[j + 1][ii] = a1; B[j + 2][ii] = a2;
+                B[j + 3][ii] = a3; B[j][ii + 4] = a4; B[j + 1][ii + 4] = a5;
+                B[j + 2][ii + 4] = a6; B[j + 3][ii + 4] = a7;
+            }
+
+            for (jj = j; jj < j + 4; jj++) {
+                a0 = A[i + 4][jj]; a1 = A[i + 5][jj]; a2 = A[i + 6][jj];
+                a3 = A[i + 7][jj]; 
+                a4 = B[jj][i + 4]; a5 = B[jj][i + 5]; a6 = B[jj][i + 6];
+                a7 = B[jj][i + 7];
+
+                B[jj][i + 4] = a0; B[jj][i + 5] = a1; B[jj][i + 6] = a2;
+                B[jj][i + 7] = a3;
+                B[jj + 4][i] = a4; B[jj + 4][i + 1] = a5; B[jj + 4][i + 2] = a6;
+                B[jj + 4][i + 3] = a7;
+            }
+
+            for (ii = i + 4; ii < i + 8; ii++) {
+                a0 = A[ii][j + 4]; a1 = A[ii][j + 5]; a2 = A[ii][j + 6];
+                a3 = A[ii][j + 7];
+                B[j + 4][ii] = a0; B[j + 5][ii] = a1; B[j + 6][ii] = a2;
+                B[j + 7][ii] = a3;
+            }
+        }
+    }
 }
 
 void transpose_61(int M, int N, int A[N][M], int B[M][N]) {
-
+    int blockSize = 16;
+    int i, j, ii, jj, tmp;
+    for (i = 0; i < N; i += blockSize) {
+        for (j = 0; j < M; j += blockSize) {
+            for (ii = i; ii < i + blockSize && ii < N; ii++) {
+                int index = -1;
+                for (jj = j; jj < j + blockSize && jj < M; jj++) {
+                    if (ii != jj) {
+                        B[jj][ii] = A[ii][jj];
+                    } else {
+                        tmp = A[ii][jj];
+                        index = ii;
+                    }
+                }
+                /* digonal conflict miss only happens when i = j */
+                if (index != -1) {
+                    B[index][index] = tmp;
+                }
+            }
+        }
+    }
 }
+char transpose_submit_desc[] = "Transpose submission";
+void transpose_submit(int M, int N, int A[N][M], int B[M][N])
+{
+    if (M == 32) {
+        transpose_32(M, N, A, B);
+    } else if (M == 64) {
+        transpose_64(M, N, A, B);
+    } else {
+        transpose_61(M, N, A, B);
+    }
+}
+
+/* 
+ * You can define additional transpose functions below. We've defined
+ * a simple one below to help you get started. 
+ */ 
 
 /* 
  * trans - A simple baseline transpose function, not optimized for the cache.
